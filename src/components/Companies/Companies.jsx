571fdd84c9d120b2d1d223b82ea8f93e24d42667
VarshneyGautam17 <96665741+VarshneyGautam17@users.noreply.github.com>
2023-08-25 18:02:23 +0530

    merged

--- src/components/Companies/Companies.jsx
+++ src/components/Companies/Companies.jsx
@@ -5,10 +5,10 @@ function Companies() {
   return (
     <section className="c-wrapper">
       <div className="paddings innerWidth flexCenter com-container">
-        <img src="./TanishqLogo.png" alt="" />
-        <img src="./malabar.png" alt="" />
-        <img src="./caratlane.png" alt="" />
-        <img src="./kalyan.png" alt="" />
+        <img src="./TanishqLogo.png" alt="Not Load" />
+        <img src="./malabar.png" alt="Not Load" />
+        <img src="./caratlane.png" alt="Not Load" />
+        <img src="./kalyan.png" alt="Not Load" />
         {/* <img src="" alt="" /> */}
       </div>
     </section>
